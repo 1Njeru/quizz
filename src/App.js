518d63268ac0ea3289102502d8d4e8f434cf812d
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 13:59:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App() {
     </div>
   )
 
- <footer className="bg-gray-900 text-white">
+ <footer name='footer' className="bg-gray-900 text-white">
       <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
         <h1
           className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
