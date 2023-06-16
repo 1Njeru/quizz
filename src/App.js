fa2247b823b281c006d7d5f15301c99da3045797
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 13:51:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -85,6 +85,23 @@ function App() {
       )}
     </div>
   );
+
+ <footer className="bg-gray-900 text-white">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+        <h1
+          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+         md:w-2/5"
+        >
+          <span className="text-teal-400">Free</span> until you're ready to
+          launch
+        </h1>
+      </div>
+      <div>
+        <span>© 2020 Appy. All rights reserved.</span>
+        <span>Terms · Privacy Policy</span>
+        <SocialIcons Icons={Icons} />
+      </div>
+    </footer>
 }
 
 export default App;
