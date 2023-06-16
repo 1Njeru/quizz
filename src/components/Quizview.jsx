1e8be2167a1e56a4c030c48dd63aff1390211671
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 15:31:42 +0300

    Update Quizview.jsx

--- src/components/Quizview.jsx
+++ src/components/Quizview.jsx
@@ -24,7 +24,7 @@ const Quizview = ({ questions, currentQuestion, handleAnswerClick }) => {
       </div>
     
        <div>
-      <footer className="bg-gray-900 text-white font-bold text-xl">
+      <footer className="bg-gray-900 text-white pt-3">
      <div className="md:flex md:justify-between md:items-center sm:px-12  bg-[#ffffff19] ">
        <span>© 2023 Appy. All rights reserved.</span>
         <span >Terms · Privacy Policy</span> </div>
