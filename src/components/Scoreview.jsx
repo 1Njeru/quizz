c2f8dde137ce5e654b5f803b617b5306ccf34668
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 15:18:25 +0300

    Update Scoreview.jsx

--- src/components/Scoreview.jsx
+++ src/components/Scoreview.jsx
@@ -6,13 +6,15 @@ const Scoreview = ({ handleResetClick, score }) => {
     <div>
       <p>You scored {score} out of 5</p>
       <button onClick={handleResetClick}>Reset</button>
-    </div>
-   
-      <footer className="bg-gray-900 text-white font-bold">
+
+   <footer className="bg-gray-900 text-white font-bold">
      <div className="md:flex md:justify-between md:items-center sm:px-12 bg-[#ffffff19] ">
        <span>Congratulations on completing</span>
         <span> @Abraham </span> </div>
     </footer>
+    </div>
+   
+      
   );
 };
 
