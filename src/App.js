830fd9ad1be9674a908b350c2d9d4c1100ed0203
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 14:43:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -88,14 +88,14 @@ function App() {
       )}
     </div>
   )
-    <div name="Footer">
+
         <footer className="bg-gray-900 text-white">
      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
        <span>© 2020 Appy. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
     </div>
     </footer>
-    </div>
+   
     
 }
 
