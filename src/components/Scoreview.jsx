855ee2e0742702c9572dcca5704a5438fee4b5c2
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 17:12:54 +0300

    Update Scoreview.jsx

--- src/components/Scoreview.jsx
+++ src/components/Scoreview.jsx
@@ -8,7 +8,10 @@ const Scoreview = ({ handleResetClick, score }) => {
       <p>You scored {score} out of 5</p>
       <button onClick={handleResetClick}>Reset</button>
 </div>
-
+ <footer className="close">
+       <span>© 2023 Appy. All rights reserved.</span>
+        <span >Terms · Privacy Policy</span> 
+    </footer>
 </div>
    
       
