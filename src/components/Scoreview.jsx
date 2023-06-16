67b71f6dabf84968f891e6aaa62d1a174b45ccf9
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 16:31:07 +0300

    Update Scoreview.jsx

--- src/components/Scoreview.jsx
+++ src/components/Scoreview.jsx
@@ -8,12 +8,13 @@ const Scoreview = ({ handleResetClick, score }) => {
       <p>You scored {score} out of 5</p>
       <button onClick={handleResetClick}>Reset</button>
 </div>
+</div>
 <div className='close'>
   <footer>
        <span>Congratulations</span>
         <span> @Njeru </span> 
     </footer>
-</div>
+
   </div>
    
       
