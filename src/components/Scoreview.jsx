89272745d45ce581cf134bb47c5f0aa617124fc6
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 15:10:43 +0300

    Update Scoreview.jsx

--- src/components/Scoreview.jsx
+++ src/components/Scoreview.jsx
@@ -9,7 +9,7 @@ const Scoreview = ({ handleResetClick, score }) => {
     </div>
      <div className='text-xl font-lighter'>
       <footer className="bg-gray-900 text-white font-bold">
-     <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7 ">
+     <div className="md:flex md:justify-between md:items-center sm:px-12 bg-[#ffffff19] ">
        <span>Congratulations on completing</span>
         <span> @Abraham </span> </div>
     </footer>
