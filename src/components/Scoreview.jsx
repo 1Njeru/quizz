63746b36a91a3378545f9b64fca7773f9beff0a0
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-16 15:51:33 +0300

    Update Scoreview.jsx

--- src/components/Scoreview.jsx
+++ src/components/Scoreview.jsx
@@ -3,7 +3,7 @@ import "./Scoreview.css";
 
 const Scoreview = ({ handleResetClick, score }) => {
   return (
-    <div className='score'>
+    <div className="score">
       <p>You scored {score} out of 5</p>
       <button onClick={handleResetClick}>Reset</button>
 <div className='close'>
